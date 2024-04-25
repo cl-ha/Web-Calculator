0091fcd84e3ae6c83ef8e2bd5c4b57497af565f5
cl-ha <168077323+cl-ha@users.noreply.github.com>
2024-04-25 22:02:54 +0700

    commit

--- Calculator_App/calculator.js
+++ Calculator_App/calculator.js
@@ -1,7 +1,13 @@
 //Hàm hiện giá trị các số/phép tính lên màn hình
 function appendToDisplay(input)
 {
-  document.getElementById('display').value += input;
+  if (flag)
+  {
+    document.getElementById('display').value = input;
+    flag = false; //đặt lại trạng thái cho biến cờ hiệu (chưa tính toán)
+  }
+  else
+    document.getElementById('display').value += input;
 }
 
 //Hàm xóa tất cả giá trị đang có trên màn hình
@@ -10,9 +16,31 @@ function clearDisplay()
   document.getElementById('display').value = '';
 }
 
-//Hàm tính toán
-function calculate()
+//Hàm tính giá trị phần trăm
+function calPercentage()
 {
-  var result = eval(document.getElementById('display').value);
-  document.getElementById('display').value = result;
+  document.getElementById('display').value = parseFloat(document.getElementById('display').value) / 100;
+  flag = true;
+}
+
+//Hàm tính số đối
+function toggleSign()
+{
+  document.getElementById('display').value = document.getElementById('display').value * -1;
+}
+
+//Hàm tính toán
+var flagResult = false; //chưa tính toán
+function calculate()
+{  
+  try
+  {
+    var result = eval(document.getElementById('display').value); 
+    document.getElementById('display').value = result;
+    flag = true; //đã tính toán
+  }
+  catch(error)
+  {
+    document.getElementById('display').value = "Error!";
+  }
 }
